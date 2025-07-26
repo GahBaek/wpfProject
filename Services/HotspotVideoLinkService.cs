90345124aa07c4add63c597d07c119fa45e121bc
Gahyun Baek <gusqorrk@gmail.com>
2025-07-26 09:57:20 +0900

    feat: spot 과 비디오 연결 로직 작성

--- Services/HotspotVideoLinkService.cs
+++ Services/HotspotVideoLinkService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShowRoomDisplay.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,16 @@ using System.Threading.Tasks;
 
 namespace ShowRoomDisplay.Services
 {
-    internal class HotspotVideoLinkService
+    /*
+     * spot 과 Video linking
+     */
+    public class HotspotVideoLinkService
     {
+        // spot 과 해당하는 video 연결하는 메소드
+        public void LinkVideo(HotspotModel hotspot, string videoPath)
+        {
+            if (hotspot == null || string.IsNullOrWhiteSpace(videoPath)) return;
+            hotspot.VideoPath = videoPath;
+        }
     }
 }
